2774f80ee4c8c895565494da880f0857761f8d7c
Julia Tavares Do Rego Michiles <juliatav@c2r5s9.42porto.com>
2026-03-13 19:23:26 +0000

    finished M04 + Flake8

--- M04/ex1/ft_archive_creation.py
+++ M04/ex1/ft_archive_creation.py
@@ -6,7 +6,7 @@ class FileError(Exception):
 
 def ft_archive_creation() -> None:
     print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
-    
+
     file_name = "new_discovery.txt"
     if file_name != "new_discovery.txt":
         raise FileError("ERROR: invalid file name!")
@@ -15,7 +15,7 @@ def ft_archive_creation() -> None:
         "[ENTRY 002] Efficiency increased by 347%\n"
         "[ENTRY 003] Archived by Data Archivist trainee\n"
         )
-    
+
     try:
         with open(file_name, "w") as file:
             file.write(text)
@@ -26,11 +26,13 @@ def ft_archive_creation() -> None:
             print("Inscribing preservation data...")
             print(content)
             print("\nData inscription complete. Storage unit sealed.")
-            print("Archive 'new_discovery.txt' ready for long-term preservation.")
+            print("Archive 'new_discovery.txt'"
+                  "ready for long-term preservation.")
     except PermissionError:
         print("ERROR: No permission to access storage vault")
     except FileError as e:
-        print("{e}")
+        print(f"{e}")
+
 
 if __name__ == "__main__":
-     ft_archive_creation()
+    ft_archive_creation()
---
+++ M04/ex1/to_do.md
@@ -0,0 +1,5 @@
+[X] Flake8
+[X] Tested
+[ ] Test everything again with diff
+[ ] Run Flake8 again
+[ ] Change initial exs to use open+close?
