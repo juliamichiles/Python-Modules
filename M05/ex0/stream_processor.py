667110c2daceb97b9320258c3d7c79e9b43f646c
Julia <root@julium.localdomain>
2026-04-28 13:36:53 +0100

    m05ex0 - basically done

--- M05/ex0/stream_processor.py
+++ M05/ex0/stream_processor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from abc import ABC
-from typing import Any, List, Dict, Union, Optional
+from abc import ABC, abstractmethod
+from typing import Any, List, Dict, Union
 
 
 class DataProcessor(ABC):
@@ -8,18 +8,17 @@ class DataProcessor(ABC):
         # empty list and idx to internally store data
         self.storage: List[tuple[int, str]] = []
         self.rank: int = 0
-   
+
     @abstractmethod
     def validate(self, data: Any) -> bool:
         # validates received data
         pass
 
-   @abstractmethod
+    @abstractmethod
     def ingest(self, data: Any) -> None:
         # processes and stores received data
         pass
 
- 
     def output(self) -> tuple[int, str]:
         if not self.storage:
             raise IndexError("No data to output!")
@@ -28,7 +27,7 @@ class DataProcessor(ABC):
 
 class NumericProcessor(DataProcessor):
     # processes ints, floats and lists of them
-    
+
     def validate(self, data: Any) -> bool:
         if isinstance(data, (int, float)):
             return True
@@ -37,17 +36,17 @@ class NumericProcessor(DataProcessor):
             if len(data) == 0:
                 return False
             for element in data:
-                if not isinstance(data, (int, float)):
+                if not isinstance(element, (int, float)):
                     return False
             return True
         else:
             return False
 
     def ingest(
-            self, 
-            data: Union[int, float, 
+            self,
+            data: Union[int, float,
             List[Union[int, float]]]
-        ) -> None:
+    ) -> None:
         if not self.validate(data):
             raise ValueError("Improper numeric data")
 
@@ -91,8 +90,8 @@ class TextProcessor(DataProcessor):
 
 class LogProcessor(DataProcessor):
     # processes log data aka a dict or a list of dicts
-    
-    def validate(self, data: Any) - bool:
+
+    def validate(self, data: Any) -> bool:
         def validate_log(log_data: Any) -> bool:
             if not isinstance(log_data, dict):
                 return False
@@ -100,10 +99,93 @@ class LogProcessor(DataProcessor):
                 if not isinstance(key, str) or not isinstance(value, str):
                     return False
             return True
-        
+
         if isinstance(data, list):
             return len(data) > 0 and all(validate_log(d) for d in data)
         return False
 
         if validate_log(data):
             return True
+
+    def ingest(
+               self,
+               data: Union[Dict[str, str], List[Dict[str, str]]]
+    ) -> None:
+
+        if not self.validate(data):
+            raise ValueError("Improper log data")
+
+        # turns data into a list if it only has one element
+        data_to_process = data if isinstance(data, list) else [data]
+
+        for element in data_to_process:
+            val_str = ": ".join(element.values())
+            self.storage.append((self.rank, val_str))
+            self.rank += 1
+
+
+def expected_demo() -> None:
+
+    print("=== Code Nexus - Data Processor ===\n")
+
+    # Numeric Processor Demo
+    print("Testing Numeric Processor...")
+    num_proc = NumericProcessor()
+    print(f" Trying to validate input '42': {num_proc.validate(42)}")
+    print(f" Trying to validate input 'Hello': {num_proc.validate('Hello')}")
+
+    print(" Test invalid ingestion of string 'foo' without prior validation:")
+    try:
+        # will generate mypy error!! (and its ok)
+        num_proc.ingest("foo")
+    except ValueError as e:
+        print(f" Got exception: {e}")
+
+    demo_num = [1, 2, 3, 4, 5]
+    print(f" Processing data: {demo_num}")
+    num_proc.ingest(demo_num)
+
+    print(" Extracting 3 values...")
+    for _ in range(3):
+        rank, value = num_proc.output()
+        print(f" Numeric value {rank}: {value}")
+
+    # Text Processor Demo
+    print("\nTesting Text Processor...")
+    txt_proc = TextProcessor()
+    demo_txt = ['Hello', 'Nexus', 'World']
+    print(f" Trying to validate input '42': {txt_proc.validate(42)}")
+    print(f" Processing data: {demo_txt}")
+    txt_proc.ingest(demo_txt)
+    print(" Extracting 1 value...")
+    rank, value = txt_proc.output()
+    print(f" Text value {rank}: {value}")
+
+    # Log Processor Demo
+    print("\nTesting Log Processor...")
+    log_proc = LogProcessor()
+    demo_log = [
+            {'log_level': 'NOTICE', 'log_message': 'Connection to server'},
+            {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}
+    ]
+
+    print(f" Trying to validate input 'Hello': {log_proc.validate('Hello')}")
+    print(f" Processing data: {demo_log}")
+    print(" Extracting 2 values...")
+
+    log_proc.ingest(demo_log)
+    for _ in range(2):
+        rank, value = log_proc.output()
+        print(f" Log entry {rank}: {value}")
+
+
+# COMMENT THE FUNCTION BELLOW BEFORE SUBMISSION!
+# def more_tests() -> None:
+    # larger and longer tests for each processor
+    # tests with empty data sets for each processor
+    # directly instantiate base class to see what happens
+
+
+if __name__ == '__main__':
+    expected_demo()
+#   more_tests() #COMMENT this line before submission
--- M05/to_do.md
+++ M05/to_do.md
@@ -3,7 +3,9 @@ _I think the main.py is from the old version of the subject_
 
 #EX0:
  [ ] Flake8 + mypy .
- [ ] Parei no validate do LogProcessor
+ [ ] Write more_tests() and better test this exercise
+ [ ] Ensure it only gets the expectes mypy error
+ [ ] Comment more_tests() function
 
 
 #OLD:
